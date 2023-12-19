7a9465e2ee97a3037d67b25d2f3d6644578bab97
Hangleang <hangleang127@gmail.com>
2023-12-19 23:41:34 +0700

    feat: extend QV base strategy contract with no defined function body

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,15 +3,15 @@ pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 
-import {Strategy} from "src/Strategy.sol";
+import {ImpactHubStrategy} from "src/ImpactHubStrategy.sol";
 
 /// @notice A very simple deployment script
 contract Deploy is Script {
     /// @notice The main script entrypoint
     /// @return strategy The deployed contract
-    function run() external returns (Strategy strategy) {
+    function run() external returns (ImpactHubStrategy strategy) {
         vm.startBroadcast();
-        strategy = new Strategy(address(0), "Greeter");
+        strategy = new ImpactHubStrategy(address(0), "Greeter");
         vm.stopBroadcast();
     }
 }
--- test/ImpactHubStrategy.t.sol
+++ test/ImpactHubStrategy.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 import "forge-std/Test.sol";
 
 // Custom Strategy
-import {Strategy} from "src/Strategy.sol";
+import {ImpactHubStrategy} from "src/ImpactHubStrategy.sol";
 
 // Strategy Interface
 import {IStrategy} from "allo/contracts/core/interfaces/IStrategy.sol";
@@ -16,12 +16,12 @@ import {RegistrySetupFull} from "allo/test/foundry/shared/RegistrySetup.sol";
 import {EventSetup} from "allo/test/foundry/shared/EventSetup.sol";
 
 contract StrategyTest is Test, EventSetup, AlloSetup, RegistrySetupFull {
-    Strategy strategy;
+    ImpactHubStrategy strategy;
 
     function setUp() public {
         __RegistrySetupFull();
         __AlloSetup(address(registry()));
 
-        strategy = new Strategy(address(allo()), "Strategy Name");
+        strategy = new ImpactHubStrategy(address(allo()), "Strategy Name");
     }
 }
